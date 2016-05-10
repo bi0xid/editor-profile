7c7ae805241ab78729e2cdea9ac6f5a2e7c9a597
Rafa Poveda <raven@raven.es>
2016-05-10 03:41:53 +0200

    Version 1.0.2 - sanitizing

--- editor-profile.php
+++ editor-profile.php
@@ -3,49 +3,50 @@
  * Plugin Name: Editor Profile
  * Plugin URI: https://wordpress.org/plugins/editor-profile
  * Description: Adds an editor profile that can edit only new posts and pages.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: bi0xid
  * Author URI: http://bi0xid.es
  * License: GPL3
-*/
-
+ */
 
 // We add the editor role with a Contributor level
 function editor_profile_add_editor_role() {
 
-add_role('editor_profile', 'Editor (non-publisher)', array( 
-    'read' => true, // True allows that capability
-    'edit_posts' => true,
-    'delete_posts' => false, // Use false to explicitly deny
-  ) );
+    add_role( 'editor_profile', 'Editor (non-publisher) ', array(
+        'read'         => true, // True allows that capability
+        'edit_posts'   => true,
+        'delete_posts' => false, // Use false to explicitly deny
+        )
+    );
 
 }
 
-add_action('admin_init', 'editor_profile_add_editor_role');
-
-
+add_action( 'admin_init', 'editor_profile_add_editor_role' );
 
 // We add the capabilities - edit and publish
 function editor_profile_add_caps() {
     // gets the editor role
     $role = get_role( 'editor_profile' );
 
-    // This only works, because it accesses the class instance.
+// This only works, because it accesses the class instance.
     // would allow the editor to edit others' posts for current theme only
-    $role->add_cap( 'edit_others_posts' ); 
-    $role->add_cap( 'edit_posts' ); 
-    $role->add_cap( 'edit_others_pages' ); 
-    $role->add_cap( 'edit_pages' ); 
-    $role->add_cap( 'edit_published_posts' ); 
-    $role->add_cap( 'edit_published_pages' ); 
+    $role->add_cap( 'edit_others_posts' );
+    $role->add_cap( 'edit_posts' );
+    $role->add_cap( 'edit_others_pages' );
+    $role->add_cap( 'edit_pages' );
+    $role->add_cap( 'edit_published_posts' );
+    $role->add_cap( 'edit_published_pages' );
 }
-add_action( 'admin_init', 'editor_profile_add_caps');
 
+add_action( 'admin_init', 'editor_profile_add_caps' );
 
 function editor_profile_disable_blocks() {
-  $user = wp_get_current_user();
-  if ( in_array( 'editor_profile', (array) $user->roles ) ) {
-    wp_enqueue_script( 'editor_profile_js', plugin_dir_url( __FILE__ ) . '/js/editor_profile.js', array(), '1.0.0', true );
-  }
+    $user = wp_get_current_user();
+
+    if ( in_array( 'editor_profile', ( array ) $user->roles ) ) {
+        wp_enqueue_script( 'editor_profile_js', plugin_dir_url(__FILE__) . '/js/editor_profile.js', array(), '1.0.0', true );
+    }
+
 }
+
 add_action( 'admin_enqueue_scripts', 'editor_profile_disable_blocks' );
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_i
 Tags: editor, profile, role
 Requires at least: 4.4
 Tested up to: 4.5.1
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -21,6 +21,10 @@ This plugin creates an editor profile that can edit new posts, but cannot edit t
 1. Activate the plugin through the 'Plugins' menu in WordPress.
 
 == Changelog ==
+
+= 1.0.2 =
+
+* Sanitizing and standarizing
 
 = 1.0.1 =
 
